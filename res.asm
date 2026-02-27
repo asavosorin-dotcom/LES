03821bfb544b69c7c14ce93f4039e345d67c8b5e
asavosorin-dotcom <asavosorin@gmail.com>
2026-02-27 18:50:30 +0300

    Прикольный баг

--- res.asm
+++ res.asm
@@ -2,7 +2,7 @@
 .code 
 
 locals @@
-; создать буфер для вывода 
+; подумать над названием буфера
 ; заносить туда строку с rx = val 
 ; потом печатать этот буфер
 ; пушить в стек все регистры, чтобы их не потерять
@@ -10,7 +10,21 @@ locals @@
 org 100h
 
 Start:
-    mov ax, 0B800h
+    mov ax, 0FFA2h
+    
+    push sp
+    push ax 
+    push bx
+    push cx
+    push si 
+    push di 
+    push bp 
+    push ds 
+    push es 
+    push ss 
+    push cs 
+    
+    mov ax, 0b800h
     mov es, ax
     
     mov di, (160 * 8 + 80)
@@ -18,7 +32,27 @@ Start:
     mov ah, 0Bh
     mov bl, 0A2h
 
-    call print_byte
+    mov cx, 5
+    lea si, Ax_equ_str
+
+    @@p:
+        lodsb
+        stosw
+        loop @@p
+
+    mov dx, 2
+    call get_asci_code_reg
+
+    mov cx, 5
+    lea si, Bx_equ_str
+
+    @@pb:
+        lodsb
+        stosw
+        loop @@p
+
+    mov dx, 4
+    call get_asci_code_reg
 
     mov ax, 4c00h
     int 21h
@@ -29,32 +63,38 @@ Start:
 ;============================================================
 
 Buffer dw 2000 dup(0)
+Ax_equ_str db 'ax = '
+Bx_equ_str db 'bx = '
 
 ;=================================================================================================
-; Start: в стеке лежат все регистры в порядке (sp, ax, bx, cx, dx, si, di, bp, ds, es, ss, cs, ip)
-;        dx - номер регистра в стеке, di - место в буфере
+; Start: в стеке лежат все регистры в порядке (sp, ax, bx, cx, dx, si, di, bp, ds, es, ss, cs)
+;        dx - номер регистра в стеке, di - место в буфере, es - сегмент буфера
 ; Destr: bx, dx, al, di, es
 ;=================================================================================================
 get_asci_code_reg proc
     
+    push di
     push bp
     mov bp, sp
-    add bp, 4
+    add bp, 28
 
     xor bx, bx
-    mov ax, cs
-    mov es, ax
+    ; mov ax, cs
+    ; mov es, ax
 
     shl dx, 1
-    add bp, dx ; bp -> value of register
-
-    mov bl, [bp]
-    call print_byte
+    sub bp, dx ; bp -> value of register
 
     mov bl, [bp + 1]
     call print_byte
 
+    mov bl, [bp]
+    call print_byte
+
     pop bp
+    pop di
+    add di, 160
+    ret
 endp
 
 ;=================================================================================================
